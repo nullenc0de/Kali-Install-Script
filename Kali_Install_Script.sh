f31c50c8b080539093cdb079e5b3c67eb0e6246b
nullenc0de <pseekamp@gmail.com>
2018-08-23 19:06:53 -0400

    Installed tools for PMKID attack

--- Kali_Install_Script.sh
+++ Kali_Install_Script.sh
@@ -162,36 +162,22 @@ echo "-------------------------------------------------------------------"
 echo "--------------- LinkedInt Installed, Next Tool! ----------------"
 echo "-------------------------------------------------------------------"
 
-export DEBIAN_FRONTEND="noninteractive"
-
-# remove previously installed Docker
-sudo apt-get remove docker docker-engine docker.io* lxc-docker*
-
-# install dependencies 4 cert
-sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-
-# add Docker repo gpg key
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-
-echo "deb https://download.docker.com/linux/debian stretch stable" >> /etc/apt/sources.list 
-
-sudo apt-get update
-
-# install Docker
-sudo apt-get install docker-ce
-
-# run Hellow World image
-sudo docker run hello-world
-
-# manage Docker as a non-root user
-sudo groupadd docker
-sudo usermod -aG docker $USER
-
-# configure Docker to start on boot
-sudo systemctl enable docker
+git clone https://github.com/ZerBea/hcxdumptool.git
+cd hcxdumptool
+make install
+cd /opt
 
 echo "-------------------------------------------------------------------"
-echo "--------------- Docker Installed, Next Tool! ----------------"
+echo "--------------- hcxdumptool Installed, Next Tool! ----------------"
+echo "-------------------------------------------------------------------"
+
+git clone https://github.com/ZerBea/hcxtools.git
+cd hcxtools
+make install
+cd /opt
+
+echo "-------------------------------------------------------------------"
+echo "--------------- hcxtools Installed, Next Tool! ----------------"
 echo "-------------------------------------------------------------------"
 
 sudo apt-get install -y libssl-dev libffi-dev python-dev build-essential
