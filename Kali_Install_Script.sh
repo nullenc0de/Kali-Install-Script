f91bfa1b6ebeb4e5fdcb57551612053cbaaa8e34
nullenc0de <pseekamp@gmail.com>
2018-11-13 22:18:48 -0500

    added http-default-accounts.nse

--- Kali_Install_Script.sh
+++ Kali_Install_Script.sh
@@ -15,6 +15,7 @@ sudo apt-get install commix -y
 sudo apt-get install sqlmap -y
 sudo apt-get install python3 -y
 pip install ldapdomaindump
+wget https://raw.githubusercontent.com/nnposter/nndefaccts/master/http-default-accounts-fingerprints-nndefaccts.lua -O /usr/share/nmap/scripts/http-default-accounts.nse
 
 cd /opt
 
