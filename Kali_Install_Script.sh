73c4af4e0c2054d5eb513127945f086302f14847
nullenc0de <pseekamp@gmail.com>
2019-11-11 10:24:37 -0500

    Update Kali_Install_Script.sh

--- Kali_Install_Script.sh
+++ Kali_Install_Script.sh
@@ -7,17 +7,72 @@
 
 apt-get update ; apt-get -y upgrade ; apt-get -y dist-upgrade ; apt-get -y autoremove ; apt-get -y autoclean ; echo
 sudo apt-get install git -y
-sudo apt-get install python3 -y
 sudo apt-get install hostapd -y
 sudo apt-get install screen -y
-sudo apt-get install fimap -y
-sudo apt-get install sshpass -y
-sudo apt-get install commix -y
-sudo apt-get install sqlmap -y
 sudo apt-get install python3 -y
 sudo apt-get install python-pip -y
 apt-get install freeradius -y
+apt-get install brutespray -y
+apt-get install -y gobuster
+apt-get install -y amass
+apt-get install -y masscan
 pip3 install ldapdomaindump
+
+#gwdomains
+export GOPATH=/opt/gwdomains
+go get -u github.com/fuzzerk/gwdomains
+ln -s /opt/gwdomains/bin/gwdomains /usr/local/bin/gwdomains
+
+#tok
+export GOPATH=/opt/tok
+go get -u github.com/tomnomnom/hacks/tok
+ln -s /opt/tok/bin/tok /usr/local/bin/tok
+
+#fff
+export GOPATH=/opt/fff
+go get -u github.com/tomnomnom/hacks/fff
+ln -s /opt/fff/bin/fff /usr/local/bin/fff
+
+# HTTProbe
+export GOPATH=/opt/httprobe
+go get -u github.com/tomnomnom/httprobe
+ln -s /opt/httprobe/bin/httprobe /usr/local/bin/httprobe
+
+# Waybackurls
+export GOPATH=/opt/waybackurls
+go get -u github.com/tomnomnom/waybackurls
+ln -s /opt/waybackurls/bin/waybackurls /usr/local/bin/waybackurls
+
+# Gf
+export GOPATH=/opt/gf
+go get -u github.com/tomnomnom/gf
+ln -s /opt/gf/bin/gf /usr/local/bin/gf
+#source /opt/gf/src/github.com/tomnomnom/gf/gf-completion.bash
+
+# Assetfinder
+export GOPATH=/opt/assetfinder
+go get -u github.com/tomnomnom/assetfinder
+ln -s /opt/assetfinder/bin/assetfinder /usr/local/bin/assetfinder
+
+# Anew
+export GOPATH=/opt/anew
+go get -u github.com/tomnomnom/anew
+ln -s /opt/anew/bin/anew /usr/local/bin/anew
+
+# Html-tool
+export GOPATH=/opt/html-tool
+go get -u github.com/tomnomnom/hacks/html-tool
+ln -s /opt/html-tool/bin/html-tool /usr/local/bin/html-tool
+
+mkdir /root/gist
+wget https://gist.githubusercontent.com/nullenc0de/96fb9e934fc16415fbda2f83f08b28e7/raw/146f367110973250785ced348455dc5173842ee4/content_discovery_nullenc0de.txt -O /root/gist/content_discovery_nullenc0de.txt
+wget https://gist.githubusercontent.com/nullenc0de/538bc891f44b6e8734ddc6e151390015/raw/a6cb6c7f4fcb4b70ee8f27977886586190bfba3d/passwords.txt -O /root/gist/passwords.txt
+wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt -O /root/gist/all.txt
+wget https://gist.githubusercontent.com/nullenc0de/9cb36260207924f8e1787279a05eb773/raw/0197d33c073a04933c5c1e2c41f447d74d2e435b/params.txt -O /root/gist/params.txt
+
+echo "-------------------------------------------------------------------"
+echo "--------------- Wordlists Installed, Next Tool! ----------------"
+echo "-------------------------------------------------------------------"
 
 cd /opt
 
@@ -244,7 +299,7 @@ git clone https://github.com/Arvanaghi/SessionGopher.git
 
 git clone https://github.com/AlessandroZ/LaZagne.git
 
-sudo git clone https://github.com/1N3/Sn1per.git
+git clone https://github.com/1N3/Sn1per.git
 cd Sn1per/
 echo "Follow Prompts!!"
 sudo ./install.sh
