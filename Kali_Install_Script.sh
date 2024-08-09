22037b3afb72548014e273a51333ee3217e0e4ad
nullenc0de <pseekamp@gmail.com>
2024-08-09 11:55:01 -0400

    Update Kali_Install_Script.sh

--- Kali_Install_Script.sh
+++ Kali_Install_Script.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-#Note, this is a script that I started to build after repeatedly needing to roll out builds. 
-#I thought about creating a custom Kali Distro, however with needing to install custom tools from github 
-#I decided on a modifiable script instead. When I started to build the script, 
-#I built upon the foundation that Matthew Clark May had used in a Repository he created, but no longer maintains. Credit where it's due.
-
-#!/bin/bash
-
 # Update and upgrade the system
 apt update && apt upgrade -y
 
 # Install required packages
-apt install -y python3-pip parallel unzip wget curl jq libpcap-dev golang-go masscan whois git libkrb5-dev 
+apt install -y python3-pip parallel unzip wget curl jq libpcap-dev golang-go masscan whois git libkrb5-dev
 pip install gssapi
 
 # Install pipx and MANSPIDER
@@ -22,6 +15,7 @@ pipx install git+https://github.com/blacklanternsecurity/MANSPIDER
 git clone https://github.com/lefayjey/linWinPwn.git
 cd linWinPwn
 bash install.sh
+cd ..
 
 # Install mitm6
 pip3 install mitm6
@@ -39,20 +33,21 @@ git clone --depth 1 https://github.com/v1s1t0r1sh3r3/airgeddon.git
 git clone https://github.com/SySS-Research/Seth.git
 cd Seth
 pip3 install -r requirements.txt
+cd ..
 
 # Clone SeeYouCM-Thief repository and install its requirements
-cd /opt
-sudo git clone https://github.com/trustedsec/SeeYouCM-Thief.git
+git clone https://github.com/trustedsec/SeeYouCM-Thief.git
 cd SeeYouCM-Thief
 python3 -m pip install -r requirements.txt
+cd ..
 
 # Clone wifite2 repository and install it
-sudo git clone https://github.com/derv82/wifite2.git
+git clone https://github.com/derv82/wifite2.git
 cd wifite2
-sudo python setup.py install
+python setup.py install
+cd ..
 
 # Clone Spray repository
-cd /opt
 git clone https://github.com/Greenwolf/Spray.git
 
 # Clone fuzzing-templates repository
@@ -62,16 +57,25 @@ git clone https://github.com/projectdiscovery/fuzzing-templates.git
 apt-get install brutespray -y
 
 # Configure Go environment variables
-GOROOT="/usr/local/go"
-PATH="${PATH}:${GOROOT}/bin"
-GOPATH=$HOME/go
-PATH="${PATH}:${GOROOT}/bin:${GOPATH}/bin"
+echo 'export GOROOT="/usr/local/go"' >> ~/.bashrc
+echo 'export PATH="${PATH}:${GOROOT}/bin"' >> ~/.bashrc
+echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+echo 'export PATH="${PATH}:${GOROOT}/bin:${GOPATH}/bin"' >> ~/.bashrc
+source ~/.bashrc
 
 # Install Go packages
 go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
-source ~/.zshrc
-pdtm -ia
 go install -v github.com/tomnomnom/hacks/kxss@latest
 go install -v github.com/Damian89/ffufPostprocessing@latest
 go install -v github.com/BishopFox/jsluice/cmd/jsluice@latest
 go install -v github.com/ffuf/ffuf/v2@latest
+
+# Install projectdiscovery tools
+pdtm -install-all
+pdtm -ua
+
+# Ensure all installed binaries are in PATH
+echo 'export PATH="${PATH}:/opt/linWinPwn:/opt/Seth:/opt/SeeYouCM-Thief:/opt/wifite2:/opt/Spray:/opt/fuzzing-templates:${HOME}/.local/bin"' >> ~/.bashrc
+
+# Reload shell configuration
+source ~/.bashrc
